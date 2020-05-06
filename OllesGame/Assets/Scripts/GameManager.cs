0561fdbc92ce54ccd4cbeebd582bd31f66b791ae
DanielYonan <64408235+DanielYonan@users.noreply.github.com>
2020-05-06 16:06:55 +0200

    .

--- OllesGame/Assets/Scripts/GameManager.cs
+++ OllesGame/Assets/Scripts/GameManager.cs
@@ -136,7 +136,7 @@ namespace Completed
 		public void GameOver()
 		{
 			//Set levelText to display number of levels passed and game over message
-			levelText.text = "After " + level + " days, you starved.";
+			levelText.text = "After " + level + " days, you died.";
 			
 			//Enable black background image gameObject.
 			levelImage.SetActive(true);
