6f10471511f8489b0a217724fe5930de6978230c
LaKiz <63583719+LaKiz@users.noreply.github.com>
2020-05-06 19:55:42 +0200

    .

--- OllesGame/Assets/Scripts/GameManager.cs
+++ OllesGame/Assets/Scripts/GameManager.cs
@@ -19,7 +19,7 @@ namespace Completed
 		private Text levelText;									//Text to display current level number.
 		private GameObject levelImage;							//Image to block out level as levels are being set up, background for levelText.
 		private BoardManager boardScript;						//Store a reference to our BoardManager which will set up the level.
-		private int level = 1;									//Current level number, expressed in game as "Day 1".
+		private int level = 0;									//Current level number, expressed in game as "Day 1".
 		private List<Enemy> enemies;							//List of all Enemy units, used to issue them move commands.
 		private bool enemiesMoving;								//Boolean to check if enemies are moving.
 		private bool doingSetup = true;							//Boolean to check if we're setting up board, prevent Player from moving during setup.
@@ -51,7 +51,7 @@ namespace Completed
 			boardScript = GetComponent<BoardManager>();
 			
 			//Call the InitGame function to initialize the first level 
-			InitGame();
+			//InitGame();
 		}
 
         //this is called only once, and the paramter tell it to be called only after the scene was loaded
@@ -70,9 +70,10 @@ namespace Completed
             instance.InitGame();
         }
 
-		
-		//Initializes the game for each level.
-		void InitGame()
+
+
+        //Initializes the game for each level.
+        void InitGame()
 		{
 			//While doingSetup is true the player can't move, prevent player from moving while title card is up.
 			doingSetup = true;
