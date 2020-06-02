995f0371e027226e822ac073455fce15b2979bab
JulleGy <64408558+JulleGy@users.noreply.github.com>
2020-06-02 16:26:49 +0200

    Revert "kk"
    This reverts commit 65f9a80bab5c4f07a320ae591353408e36250522.

--- OllesGame/Assets/Scripts/MainMenu.cs
+++ OllesGame/Assets/Scripts/MainMenu.cs
@@ -2,15 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-//using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
 
     public void StartGame()
-    {
-        
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    {        
+            
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
 
